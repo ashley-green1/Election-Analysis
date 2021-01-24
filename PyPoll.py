4bfd835e933b594a96936228f5b9bc756bbd1757
Ashley Green <75928657+ashley-green1@users.noreply.github.com>
2021-01-24 16:26:01 -0600

    Update
    update

--- PyPoll.py
+++ PyPoll.py
@@ -23,8 +23,14 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 
 
-# 1. Initialize a total vote counter.
+# Initialize a total vote counter.
 total_votes = 0
+
+# Create variable for list of Candidate Options.
+candidate_options = []
+
+# Declare the empty dictionary for candidate votes.
+candidate_votes = {}
 
 
 
@@ -41,10 +47,36 @@ with open(file_to_load) as election_data:
 
     # Print (Iterate) each row in the CSV file.
     for row in file_reader:
-        # 2. Add to the total vote count.
+        # Add to the total vote count.
         total_votes += 1
 
-# 3. Print the total votes.
-print(total_votes)
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidates vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+
+# Print the candidate list.
+print(candidate_votes)
 
 
