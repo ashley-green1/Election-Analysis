1c8c51e4bb00d89458a59980ccdb892e0a63f291
Ashley Green <75928657+ashley-green1@users.noreply.github.com>
2021-01-24 13:51:38 -0600

    Update PyPoll.py
    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -16,46 +16,23 @@ print("The time right now is ", now)
 import csv
 import os
 
-# Create a variable and assign the path to extract from Election Results.
+# Assign a variable for the file to load and the indirect path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-# Create a variable and assign the path to save analysis to Election Analysis.
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Print the file object.
+     print(election_data)
+
+
+# Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Create a total vote counter and initialize it to 0.
-total_votes = 0
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
 
-# Candidate Options List
-candidate_options = []
+    # Write three counties to the file.
+    txt_file.write("Counties in the Election\n_________________________\n")
+    txt_file.write("Arapahoe\nDenver\nJefferson")
 
-# Declare an empty dictionary.
-candidate_votes = {}
 
-# Open the election results and read the file.
-with open(file_to_load, 'r') as election_data:
-    # Read the Election Results file.
-    file_reader = csv.reader(election_data)
-
-    # Read and print the header row.
-    headers = next(file_reader)
-    
-    # Print each row of the Election Results file.
-    for row in file_reader:
-        # Add to the total vote count.
-        total_votes +=1
-
-        # Print the candidate name from each row.
-        candidate_name = row[2]
-
-        # If the candidate does not match any existing canditate...
-        if candidate_name not in candidate_options:
-            # Add it to the list of candidates.
-            candidate_options.append(candidate_name)
-
-            # Begin tracking that candidate's vote count.
-            candidate_votes[candidate_name] = 0
-
-        # Add a vote to that candidate's count.
-        candidate_votes[candidate_name] += 1
-
-# Print the candidate vote dictionary.
-print(candidate_votes)
